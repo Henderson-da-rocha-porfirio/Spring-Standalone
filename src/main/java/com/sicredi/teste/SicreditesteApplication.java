795bae14e95fca2589e457513e185ee80cea2f49
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-07-27 12:14:31 -0300

    COMMIT 1 master

---
+++ src/main/java/com/sicredi/teste/SicreditesteApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicreditesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicreditesteApplication.class, args);
+	}
+
+}
