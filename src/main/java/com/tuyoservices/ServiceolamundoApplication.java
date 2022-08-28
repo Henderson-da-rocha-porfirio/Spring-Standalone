f9eaeda486a50e7a3a55704fc6dfa4c1709d5798
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-08-28 17:51:18 -0300

    #Commit 1 serviceolamundo

---
+++ src/main/java/com/tuyoservices/ServiceolamundoApplication.java
@@ -0,0 +1,13 @@
+package com.tuyoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceolamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceolamundoApplication.class, args);
+	}
+
+}
