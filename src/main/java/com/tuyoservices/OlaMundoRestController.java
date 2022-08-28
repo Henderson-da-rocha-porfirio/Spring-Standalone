944d4e6e6845a01ad5452a91069021ffe4b5b555
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-08-28 18:46:19 -0300

    #Commit 2 serviceolamundo

---
+++ src/main/java/com/tuyoservices/OlaMundoRestController.java
@@ -0,0 +1,9 @@
+package com.tuyoservices;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/* @RestController: é a anotação que devemos por quando queremos construir aplicações Rest. */
+
+@RestController
+public class OlaMundoRestController {
+}
